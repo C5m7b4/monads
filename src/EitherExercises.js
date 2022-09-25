f7f523e6098f22bc0d6abc9a68e8ec5070c2a00e
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-25 14:22:13 -0500

    Either

--- src/EitherExercises.js
+++ src/EitherExercises.js
@@ -1,11 +1,30 @@
-const eitherex1 = () => {
-  const findColor = (name) =>
-    ({ red: '#ff4444', blue: '#3b5988', yellow: '#fff68f' }[
-      name
-    ]);
+import { Right, Left } from './Either';
 
-  const res = findColor('red').toUpperCase();
-  console.log(res);
+const eitherex1 = () => {
+  // const findColor_ = (name) =>
+  //   ({ red: '#ff4444', blue: '#3b5988', yellow: '#fff68f' }[
+  //     name
+  //   ]);
+
+  const findColor = (name) => {
+    const found = {
+      red: '#ff4444',
+      blue: '#3b5988',
+      yellow: '#fff68f',
+    }[name];
+    return found ? Right(found) : Left('not found');
+  };
+
+  const res = () =>
+    findColor('red')
+      .map((x) => x.toUpperCase())
+      .map((x) => x.slice(1))
+      .fold(
+        () => 'no color',
+        (color) => color
+      );
+
+  console.log(res());
 };
 
 export { eitherex1 };
