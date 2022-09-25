b2f05da7d9ebf4a2cac80da7319c3dad35e454b7
C5m7b4 <mbedingfield@dcrpos.com>
2022-09-25 14:55:57 -0500

    Add trycatch function

--- src/index.js
+++ src/index.js
@@ -10,18 +10,22 @@ child.innerHTML = 'Hello';
 main.appendChild(child);
 root.appendChild(main);
 
-import {
-  exercise1,
-  exercise2,
-  exercise3,
-  exercise4,
-  exercise5,
-} from './BoxExercises';
-exercise1();
-exercise2();
-exercise3();
-exercise4();
-exercise5();
+// import {
+//   exercise1,
+//   exercise2,
+//   exercise3,
+//   exercise4,
+//   exercise5,
+// } from './BoxExercises';
+// exercise1();
+// exercise2();
+// exercise3();
+// exercise4();
+// exercise5();
 
-import { eitherex1 } from './EitherExercises';
-eitherex1();
+// import { eitherex1 } from './EitherExercises';
+// eitherex1();
+
+import getPort from './excercises_01';
+const result = getPort();
+console.log(result);
